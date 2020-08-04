f40234e439d012849b638f29a5e3e4f6c681cd51
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-04 17:00:15 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,11 +4,11 @@ def hello(array)
   i = 0
   collection = []
   while i < array.length
-  
-    binding.pry
+    collection <<
     yield(array[i])
     i += 1
   end
+  collection 
 end
 
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
